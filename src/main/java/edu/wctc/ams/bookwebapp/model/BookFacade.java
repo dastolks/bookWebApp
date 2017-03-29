2be59473f072d24f2a2456b63d70151cc58aecb7
aschindler1 <aschindler1@L11119042801.wctc.msft>
2017-03-29 12:48:27 -0500

    new books! Add CRUD to it.

---
+++ src/main/java/edu/wctc/ams/bookwebapp/model/BookFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wctc.ams.bookwebapp.model;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author aschindler1
+ */
+@Stateless
+public class BookFacade extends AbstractFacade<Book> {
+
+    @PersistenceContext(unitName = "edu.wctc.ams_bookWebApp_war_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public BookFacade() {
+        super(Book.class);
+    }
+    
+}
